f84372d11b12b79253711a66a8c1c6243cb7a53d
872447993@qq.com <872447993@qq.com>
2019-11-07 17:17:28 +0800

    first commit

--- Example/TimeUIList/ViewController.swift
+++ Example/TimeUIList/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let v =   OneTimeView.sharedInstance()
+//        let v =   OneTimeView.sharedInstance()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
